2d93b0e0713bb4e3c2ae0439f00046839f61267c
Gabrielstwr <gabriel.steinwander@josephinum.at>
2023-08-31 11:18:08 +0200

    Aktueller ESP-Code

---
+++ ESP/AKTUELL/Wifi/Wifi.ino
@@ -0,0 +1,114 @@
+
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <Wire.h>
+#include <String.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BME680.h"
+
+Adafruit_BME680 bme; // I2C
+
+#include "arduino_secrets.h"
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;  // your network SSID (name)
+char pass[] = SECRET_PASS;  // your network password (use for WPA, or use as key for WEP)
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+WiFiUDP Udp;
+unsigned int localPort = 8585;      // Portnummer
+String packet_Temp;
+String packet_Humidity;
+
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println(F("BME680 async test"));
+
+  if (!bme.begin()) {
+    Serial.println(F("Could not find a valid BME680 sensor, check wiring!"));
+    while (1);
+  }
+
+  // Set up oversampling and filter initialization
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  initWiFi();
+  Serial.print("RRSI: ");
+  Serial.println(WiFi.RSSI());
+  printWifiStatus();
+}
+
+void loop() {
+   // Tell BME680 to begin measurement.
+  unsigned long endTime = bme.beginReading();
+  if (endTime == 0) {
+    Serial.println(F("Failed to begin reading :("));
+    return;
+  }
+  Serial.print(F("Reading started at "));
+  Serial.print(millis());
+  Serial.print(F(" and will finish at "));
+  Serial.println(endTime);
+
+  Serial.println(F("You can do other work during BME680 measurement."));
+  delay(50); // This represents parallel work.
+  
+  if (!bme.endReading()) {
+    Serial.println(F("Failed to complete reading :("));
+    return;
+  }
+  Serial.print(F("Reading completed at "));
+  Serial.println(millis());
+
+  Serial.print(F("Temperature = "));
+  Serial.print(bme.temperature);
+  Serial.println(F(" *C"));
+
+  Serial.print(F("Humidity = "));
+  Serial.print(bme.humidity);
+  Serial.println(F(" %"));
+
+  packet_Temp = String(bme.temperature);
+  packet_Humidity = String(bme.humidity);
+
+    //##Packet senden##
+    Udp.beginPacket("172.16.2.19", localPort);     // hier die IP-Adresse vom "IPTest.ino" festlegen
+    Udp.print(packet_Temp);
+    Serial.println("Packet Temperatur gesendet");
+    Udp.endPacket();
+
+    Udp.beginPacket("172.16.2.19", localPort);     // hier die IP-Adresse vom "IPTest.ino" festlegen
+    Udp.print(packet_Humidity);
+    Serial.println("Packet Feuchtigkeit gesendet");
+    Udp.endPacket();
+    
+  Serial.println();
+  delay(2000);
+}
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
---
+++ ESP/AKTUELL/Wifi/arduino_secrets.h
@@ -0,0 +1,2 @@
+#define SECRET_SSID "WiFi-Stones"    // HUAWEI_B535_9771
+#define SECRET_PASS "stones4net"    //MNF9A7Ht3TM
